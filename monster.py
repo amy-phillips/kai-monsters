02bc02ab4286e089655e53ed5cf540224d1db1d2
amy-phillips <amy@islovely.co.uk>
2025-04-10 20:01:46 +0100

    add monsters

--- monster.py
+++ monster.py
@@ -23,6 +23,23 @@ class Button:
                     return True
         return False
 
+class Monster:
+    def __init__(self, name, health, might, y_offset):
+        self.name = name
+        self.health = health
+        self.might = might
+        self.y_offset = y_offset
+
+    def render(self, screen):
+        font = pygame.font.Font(pygame.font.get_default_font(), 30)
+        health_surface = font.render(f"{self.name} - Health: {self.health} Might: {self.might}", False, (0, 0, 0))
+        screen.blit(health_surface, (0,self.y_offset))
+
+    def calculate_attack_damage(self):
+        return self.might #TODO this will depend on move selected etc
+    def sustain_attack_damage(self, damage):
+        self.health -= damage
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -30,6 +47,8 @@ clock = pygame.time.Clock()
 running = True
 
 move_button = Button([350, 50], "Do Epic Move", [50, 50])
+attacking_monster = Monster("attack", 1000, 30, 0)
+defending_monster = Monster("defend", 1000, 30, 30)
 
 while running:
     # poll for events
@@ -46,6 +65,11 @@ while running:
     move_button.render(screen)
     if move_button.clicked(events):
         print("Doing epic move...")
+        damage = attacking_monster.calculate_attack_damage()
+        defending_monster.sustain_attack_damage(damage)
+
+    attacking_monster.render(screen)
+    defending_monster.render(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
